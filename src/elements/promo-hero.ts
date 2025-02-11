518d443dd2352a929edfc8ceb75240e9059d2595
Glenn Vandeuren (aka Iondependent) <vandeurenglenn@gmail.com>
2025-02-11 23:46:42 +0100

    Update promo-hero.ts

--- src/elements/promo-hero.ts
+++ src/elements/promo-hero.ts
@@ -1,6 +1,7 @@
 import { html, css, customElement, LiteElement, property } from '@vandeurenglenn/lite'
 import '@vandeurenglenn/flex-elements/column.js'
 import '@vandeurenglenn/lite-elements/icon-button.js'
+import '@vandeurenglenn/lite-elements/divider.js'
 
 @customElement('promo-hero')
 export class PromoHero extends LiteElement {
@@ -29,7 +30,7 @@ export class PromoHero extends LiteElement {
       }
 
       .container {
-        padding: 12px;
+        padding: 12px 12px 24px 12px;
         box-sizing: border-box;
         max-width: 400px;
         width: 100%;
@@ -59,6 +60,9 @@ export class PromoHero extends LiteElement {
         display: flex;
         flex-direction: column;
         gap: 12px;
+
+        box-sizing: border-box;
+        padding: 0 48px;
       }
 
       custom-icon-button {
@@ -68,7 +72,35 @@ export class PromoHero extends LiteElement {
       }
 
       [icon='redeem'] {
-        --custom-icon-size: 64px;
+        --custom-icon-size: 80px;
+        position: absolute;
+        left: 12px;
+        top: 12px;
+        transform: rotate(-45deg);
+      }
+
+      h1 {
+        font-size: 60px;
+        margin-top: 0;
+        margin-bottom: 0;
+      }
+
+      .off-container {
+        align-items: center;
+        gap: 12px;
+        width: 100%;
+        font-size: 32px;
+        box-sizing: border-box;
+        padding: 0 48px;
+      }
+
+      .off-container h2 {
+        margin: 0;
+        text-transform: uppercase;
+      }
+
+      p {
+        font-size: 24px;
       }
     `
   ]
@@ -79,11 +111,18 @@ export class PromoHero extends LiteElement {
           icon="close"
           @click=${() => this.remove()}></custom-icon-button>
 
-        <h2>ACTIE!</h2>
+        <h2>HNM Hairsystems</h2>
 
         <flex-column>
           <custom-icon icon="redeem"> </custom-icon>
-          <p>schrijf je nu in voor onze nieuwsbrief en ontvang 10% korting op uw eerste aankoop in de onlineshop</p>
+          <h1>10%</h1>
+          <flex-row class="off-container">
+            <custom-divider></custom-divider>
+            <h2>off</h2>
+
+            <custom-divider></custom-divider>
+          </flex-row>
+          <p>Registreer als nieuwe klant en ontvang 10% korting op uw eerste aankoop.</p>
 
           <custom-button
             type="tertiary"
